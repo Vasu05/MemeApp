5781eb410dc61613f253c7626c5b7d4949b98ac5
Vasu Chand <Vasu.chand@mobikwik.com>
2018-10-28 15:10:42 +0530

    added extention class

--- MemeApp/MemeHomePageVC.swift
+++ MemeApp/MemeHomePageVC.swift
@@ -24,6 +24,8 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
     @IBOutlet weak var mDownloadBtn: UIButton!
     @IBOutlet weak var mMemedView: UIView!
     
+    var memedImage :UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mTopTextField.delegate = self
@@ -34,77 +36,11 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
         super.viewWillAppear(true)
         mCameraBtn.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         
-        self.suscriberToKeyBoardNotification()
+        suscriberToKeyBoardNotification()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        self.unsuscriberToKeyBoardNotification()
-    }
-    
-    // MARK: Camera , Photos , File Saving Acesss Permission
-    
-    func cameraPermission()  {
-        
-        
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-            case .authorized:
-               self.openImagePickerController(mCameraBtn)
-                
-                // The user has previously granted access to the camera.
-                break;
-            
-            case .notDetermined: // The user has not yet been asked for camera access.
-            AVCaptureDevice.requestAccess(for: .video) { granted in
-                if granted {
-                    self.openImagePickerController(self.mCameraBtn)
-
-                   // self.setupCaptureSession()
-                }
-            }
-            break;
-            case .denied:
-                 showDialog(title: "Error", desc: "Provide Camera Acess Permission")
-                 print("no camera permission ")  // The user has previously denied access.
-            return
-            case .restricted:
-                showDialog(title: "Error", desc: "Provide Camera Acess Permission")
-
-                print("no camera permission ") // The user can't grant access due to restrictions.
-            return
-        }
-        
-        
-    }
-    
-    func photoPermission()  {
-        switch PHPhotoLibrary.authorizationStatus() {
-            
-        case .notDetermined:
-            print("NotDetermined")
-            PHPhotoLibrary.requestAuthorization { (status:PHAuthorizationStatus) -> Void in
-                print("photos permission granted")
-                self.openImagePickerController(self.mAlbumsBtn)
-            }
-            break
-            
-        case .authorized:
-            print("PHAuthorizationStatus.Authorized")
-            print("photos permission granted")
-            self.openImagePickerController(self.mAlbumsBtn)
-            break
-            
-            
-        case .restricted:
-            print("PHAuthorizationStatus.Restricted")
-            break
-        case .denied:
-            showDialog(title: "Photos", desc: "Provide Photos Acess Permission")
-
-            print("PHAuthorizationStatus.Denied")
-           
-            break
-       
-        }
+        unsuscriberToKeyBoardNotification()
     }
     
     
@@ -123,36 +59,7 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
     
     
     
-    //MARK :KEYBOARD
-    
-    func getKeyBoardHeight(_ notification:Notification) -> CGFloat {
-        let userInfo = notification.userInfo
-        let keyBoardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
-        return keyBoardSize.cgRectValue.height
-        
-    }
-    
-    @objc func keyboardWillShow(_ notification:Notification) {
-        
-        if mBottomTextField.isFirstResponder{
-            view.frame.origin.y -= getKeyBoardHeight(notification)
-
-        }
-    }
-    
-    func keyboardWillHide()  {
-        view.frame.origin.y = 0
-    }
-    
-    func suscriberToKeyBoardNotification()  {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name:UIResponder.keyboardWillShowNotification, object: nil)
-    }
-    
-    func unsuscriberToKeyBoardNotification()  {
-        
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        
-    }
+  
     
     // MARK: TextFieldDelegate
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
@@ -162,7 +69,7 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        self.keyboardWillHide()
+        keyboardWillHide()
         return true;
     }
     
@@ -174,7 +81,7 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
     
     @IBAction func cameraBtnPressed(_ sender: Any) {
        
-        self.cameraPermission()
+        cameraPermission()
         
     }
     @IBAction func shareBtnPressed(_ sender: Any) {
@@ -185,6 +92,15 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
             let items = [image]
             
             let activityController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+            
+            activityController.completionWithItemsHandler = {
+                (type,completed,items,error) in
+                
+                if error==nil{
+                    self.save()
+                }
+                
+            }
             self.present(activityController,animated: true,completion: nil)
         }
         else{
@@ -219,22 +135,13 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
         }
     }
     
-    func generateMemedImage() -> UIImage {
-        
-        
-        UIGraphicsBeginImageContext(self.mMemedView.frame.size)
-        mMemedView.drawHierarchy(in: self.mMemedView.frame, afterScreenUpdates: true)
-        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        return memedImage
-    }
+    
     
     
     @IBAction func  saveImage(_ sender: Any)  {
         
         if  mImageView.image != nil{
-            let memedImage = generateMemedImage()
+            memedImage = self.generateMemedImage()
         UIImageWriteToSavedPhotosAlbum(memedImage,self ,#selector(image(_:didFinishSavingWithError:contextInfo:)),nil)
             
         }else{
@@ -242,23 +149,6 @@ class MemeHomePageVC: UIViewController,UITextFieldDelegate,UIImagePickerControll
         }
     }
     
-    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        
-        if  error == nil  {
-            showDialog(title: "Success", desc: "Image saved successfully ! ")
-        }else{
-            showDialog(title: "Error", desc: "Error Occured while saving photos ! ")
-
-        }
-    }
-    
-    
-    func showDialog(title:String,desc:String)  {
-        
-        let alert = UIAlertController(title: title, message: desc, preferredStyle: .alert)
-        alert.addAction( UIAlertAction(title: "OK", style:.default ))
-        present(alert,animated: true)
-    }
-    
+  
 }
 
